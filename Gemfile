fd70bdd6605fe0d455ccd41782091647d0941563
Iuliia <julika27@gmail.com>
2019-10-28 20:24:13 +0000

    Set app Google OAuth, added basic bootstrap styles

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use postgres as the database for Active Record
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -60,3 +60,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'jquery-rails'
+gem 'bootstrap', '~> 4.3.1'
+gem 'devise'
+gem 'simple_form'
+gem 'omniauth-google-oauth2'
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+
+ def self.from_omniauth(auth)
+
+   # # Either create a User record or update it based on the provider (Google) and the UID
+   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+     user.email = auth.info.email
+     user.password = Devise.friendly_token[0, 20]
+     user.token = auth.credentials.token
+     user.expires = auth.credentials.expires
+     user.expires_at = auth.credentials.expires_at
+     user.refresh_token = auth.credentials.refresh_token
+   end
+ end
+end
