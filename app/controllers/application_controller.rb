dc79a54bd79a9c729d5fc23060d318677b96315f
Iuliia <julika27@gmail.com>
2019-10-29 15:47:41 +0000

    New workout form date and time formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { request.format.json? }
+  skip_before_action :verify_authenticity_token
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :workouts
+  has_many :playlists
 
 
  def self.from_omniauth(auth)
